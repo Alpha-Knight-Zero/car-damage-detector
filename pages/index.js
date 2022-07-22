f863a1a1491eaf114917f49e9aad4d19d478b5d6
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2022-07-22 18:24:36 +0530

    removed obselete icons

--- pages/index.js
+++ pages/index.js
@@ -190,48 +190,6 @@ export default function Home() {
 										</a>
 									</div>
 								</div>
-								<div className='flex -mr-2 md:hidden'>
-									<button className='inline-flex items-center justify-center p-2 text-gray-800 rounded-md dark:text-white hover:text-gray-300 focus:outline-none'>
-										<svg
-											width='20'
-											height='20'
-											fill='currentColor'
-											className='w-8 h-8'
-											viewBox='0 0 1792 1792'
-											xmlns='http://www.w3.org/2000/svg'
-										>
-											<path d='M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z'></path>
-										</svg>
-									</button>
-								</div>
-							</div>
-						</div>
-						<div className='md:hidden'>
-							<div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-								<a
-									className='block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-gray-800 dark:hover:text-white'
-									href='/#'
-								>
-									Home
-								</a>
-								<a
-									className='block px-3 py-2 text-base font-medium text-gray-800 rounded-md dark:text-white'
-									href='/#'
-								>
-									Gallery
-								</a>
-								<a
-									className='block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-gray-800 dark:hover:text-white'
-									href='/#'
-								>
-									Content
-								</a>
-								<a
-									className='block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-gray-800 dark:hover:text-white'
-									href='/#'
-								>
-									Contact
-								</a>
 							</div>
 						</div>
 					</nav>
